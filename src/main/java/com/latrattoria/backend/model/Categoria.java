a977d0e567dc53a02afdc1c9cc73b84dd9343bd7
alvarorc13 <alvarorioscallejas09@gmail.com>
2026-02-10 11:05:00 +0100

    Anadiendo modelo Categoria

---
+++ src/main/java/com/latrattoria/backend/model/Categoria.java
@@ -0,0 +1,19 @@
+package com.latrattoria.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categoria")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String nombre;
+}
